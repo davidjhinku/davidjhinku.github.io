357831f33d20e980d3f0ff40f7843e7a23d896eb
David <davidjhinku@gmail.com>
2021-03-01 21:18:46 -0500

    Completed form and status styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -116,3 +116,52 @@
 			});
 
 })(jQuery);
+
+//Prevent form redirect
+window.addEventListener("DOMContentLoaded", function () {
+
+	// get the form elements defined in your form HTML above
+
+	var form = document.getElementById("my-form");
+	// var button = document.getElementById("my-form-button");
+	var status = document.getElementById("status");
+
+	// Success and Error functions for after the form is submitted
+
+	function success() {
+		form.reset();
+		// button.style = "display: none ";
+		status.classList.add('success')
+		status.innerHTML = "Sent, thank you!";
+	}
+
+	function error() {
+		status.classList.add('error')
+		status.innerHTML = "Oops! There was a problem.";
+	}
+
+	// handle the form submission event
+
+	form.addEventListener("submit", function (e) {
+		e.preventDefault();
+		var data = new FormData(form);
+		ajax(form.method, form.action, data, success, error);
+	});
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+	var xhr = new XMLHttpRequest();
+	xhr.open(method, url);
+	xhr.setRequestHeader("Accept", "application/json");
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState !== XMLHttpRequest.DONE) return;
+		if (xhr.status === 200) {
+			success(xhr.response, xhr.responseType);
+		} else {
+			error(xhr.status, xhr.response, xhr.responseType);
+		}
+	};
+	xhr.send(data);
+}
